0128889402a621dc501121dbf538c8ad5994e898
Jumpei Onishi <waibandl321@gmail.com>
2023-12-12 17:06:36 +0900

    storage追加

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s334cc9079": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
