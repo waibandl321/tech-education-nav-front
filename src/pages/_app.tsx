faabb6eff78d0886097aadb1379331511070e8ff
Jumpei Onishi <waibandl321@gmail.com>
2023-12-28 16:31:05 +0900

    NProgress実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,13 @@ import type { AppProps } from "next/app";
 import { MessageAlertProvider } from "@/contexts/MessageAlertContext";
 import { AccountProvider } from "@/contexts/AccountContext";
 import { LoadingProvider } from "@/contexts/LoadingContext";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
