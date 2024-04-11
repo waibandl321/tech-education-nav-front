4436f87a2b1de12a9bbc4e4e7d597aaadd891002
kp-jumpei-onishi <kpjumpeionishi@gmail.com>
2024-04-11 18:27:05 +0900

    next-redux-wrapperを削除

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "@/lib/features/counter/counterSlice";
 import searchDataReducer from "@/lib/features/counter/searchDataSlice";
-import { createWrapper, HYDRATE } from "next-redux-wrapper";
 
 // ストアの初期化
 export const initializeStore = () => {
@@ -10,16 +9,8 @@ export const initializeStore = () => {
       counter: counterReducer,
       searchData: searchDataReducer,
     },
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({ serializableCheck: false }),
   });
 };
-
-export const reduxWrapper = createWrapper<AppStore>(initializeStore, {
-  debug: true,
-  serializeState: (state) => JSON.stringify(state),
-  deserializeState: (state) => JSON.parse(state),
-});
 
 export type AppStore = ReturnType<typeof initializeStore>;
 // ストア自体から `RootState` 型と `AppDispatch` 型を推測する
--- src/pages/test/redux/index.tsx
+++ src/pages/test/redux/index.tsx
@@ -1,44 +1,31 @@
 import React from "react";
 import { withCommonServerSideProps } from "@/hooks/server/withCommonServerSideProps";
-import { reduxWrapper } from "@/lib/store";
 import { fetchSearchPageData } from "@/hooks/server/fetchData";
-import { setData } from "@/lib/features/counter/searchDataSlice";
-import { NextPage } from "next";
-import dynamic from "next/dynamic";
-import { increment } from "@/lib/features/counter/counterSlice";
+import { useSearchDataSelector } from "@/lib/features/counter/searchDataSlice";
 
-const NextReduxWrapper = dynamic(
-  () => import("@/components/test/NextReduxWrapper")
-);
+export default function TestReduxPage() {
+  const postData = useSearchDataSelector();
+  return <div>{JSON.stringify(postData)}</div>;
+}
 
-type NextReduxWrapperPageProps = {};
-const NextReduxWrapperPage: NextPage<NextReduxWrapperPageProps> = ({}) => {
-  return <NextReduxWrapper />;
-};
+export const getServerSideProps = withCommonServerSideProps(async (context) => {
+  const result = await fetchSearchPageData();
 
-export const getServerSideProps = reduxWrapper.getServerSideProps((store) =>
-  withCommonServerSideProps(async (context) => {
-    const result = await fetchSearchPageData();
-    store.dispatch(setData(result));
-    store.dispatch(increment());
-    return {
-      props: {
-        centers: result.centers,
-        courses: result.courses,
-        languages: result.languages,
-        frameworks: result.frameworks,
-        libraries: result.libraries,
-        developmentTools: result.developmentTools,
-        jobTypes: result.jobTypes,
-        paymentMethods: result.paymentMethods,
-        creditCards: result.creditCards,
-        developmentCategories: result.developmentCategories,
-        developmentProducts: result.developmentProducts,
-        qualifications: result.qualifications,
-        benefitUserCategories: result.benefitUserCategories,
-      },
-    };
-  })
-);
-
-export default NextReduxWrapperPage;
+  return {
+    props: {
+      centers: result.centers,
+      courses: result.courses,
+      languages: result.languages,
+      frameworks: result.frameworks,
+      libraries: result.libraries,
+      developmentTools: result.developmentTools,
+      jobTypes: result.jobTypes,
+      paymentMethods: result.paymentMethods,
+      creditCards: result.creditCards,
+      developmentCategories: result.developmentCategories,
+      developmentProducts: result.developmentProducts,
+      qualifications: result.qualifications,
+      benefitUserCategories: result.benefitUserCategories,
+    },
+  };
+});
