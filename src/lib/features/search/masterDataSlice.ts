cab064186d143650cfb3d9f12e78ec19830bac4b
kp-jumpei-onishi <kpjumpeionishi@gmail.com>
2024-06-09 18:21:00 +0900

    masterDataSliceをリファクタリング

--- src/lib/features/search/masterDataSlice.ts
+++ src/lib/features/search/masterDataSlice.ts
@@ -1,11 +1,9 @@
-import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { ReduxRootState } from "@/lib/store";
 import { MasterDataMap } from "@/types/CommonType";
 
 export type MasterDataSliceState = {
   data: MasterDataMap;
-  status: "idle" | "pending" | "succeeded" | "failed";
-  error: undefined | string;
 };
 
 const initialState: MasterDataSliceState = {
@@ -21,14 +19,8 @@ const initialState: MasterDataSliceState = {
     developmentCategories: [],
     benefitUserCategories: [],
   },
-  status: "idle",
-  error: undefined,
 };
 
-interface SetMasterObjectPayloadAction {
-  key: keyof MasterDataMap;
-  item: any;
-}
 interface SetMasterArrPayloadAction {
   key: keyof MasterDataMap;
   items: any[];
@@ -41,26 +33,11 @@ const masterDataSlice = createSlice({
     setMasterArr: (state, action: PayloadAction<SetMasterArrPayloadAction>) => {
       state.data[action.payload.key] = action.payload.items;
     },
-    setMasterObject: (state, action: PayloadAction<SetMasterObjectPayloadAction>) => {
-      const existItemIndex = state.data[action.payload.key].findIndex(
-        (v) => v._id === action.payload.item._id
-      );
-      if (existItemIndex === -1) {
-        // データが存在しない場合は新規追加
-        state.data[action.payload.key].push(action.payload.item);
-      } else {
-        // データが存在する場合は更新
-        state.data[action.payload.key][existItemIndex] = {
-          ...state.data[action.payload.key][existItemIndex],
-          ...action.payload.item,
-        };
-      }
-    },
   },
 });
 
 export const selectMasterData = (state: ReduxRootState) => state.searchData;
 
-export const { setMasterObject, setMasterArr } = masterDataSlice.actions;
+export const { setMasterArr } = masterDataSlice.actions;
 
 export default masterDataSlice.reducer;
